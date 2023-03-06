ae84297de2eb2e9bc4f778c4c9ef83e06c273acb
Elena Chechulina <denis.chechulin.31@gmail.com>
2023-03-05 19:32:33 -0500

    modified Bank-website script and Bank-App html

--- StabilityFirst-Bank-website /script.js
+++ StabilityFirst-Bank-website /script.js
@@ -38,7 +38,88 @@ document.addEventListener('keydown', function (e) {
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function(e) {
+btnScrollTo.addEventListener('click', function (e) {
   const section1Coords = section1.getBoundingClientRect();
-  console
+  console.log(section1Coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log(
+    'Сurrent scroll: x, y',
+    window.pageXOffset,
+    window.pageYOffset
+  );
+  console.log(
+    'Width and height of viewport',
+    document.documentElement.clientWidth,
+    document.documentElement.clientHeight
+  );
+
+  // window.scrollTo(
+  //   section1Coords.left + window.pageXOffset,
+  //   section1Coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: section1Coords.left + window.pageXOffset,
+  //   top: section1Coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Event Delegation
+// Add event listener for common parent
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // Define target element
+  console.log(e.target);
+  if (e.target.classList.contains('nav__link')) {
+    const href = e.target.getAttribute('href');
+    console.log(href);
+    document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Tabs
+const tabs = document.querySelectorAll('operations__tab');
+const tabContainer = document.querySelector('operations__tab-container');
+const tabContents = document.querySelectorAll('operations__content')
+
+tabContainer.addEventListener('click', function(e) {
+  const clickedButton = e.target.closest('.operations__tab');
+  if(!clickedButton) return;
+
+  // Active tab
+  tabs.forEach(tab => tab.remove.
+    classList('operations__tab--active'));
+    clickedButton.classList.add
+    ('operations__tab--active');
+
+    // Active content
+    tabContents.forEach(content => 
+      content.classList.remove
+      ('operations__content--active')
+      );
+      document.querySelector(`operations__content--${clickedButton.dataset.tab}`)
+      .classList.add('operations__content--active');
 })
+
+// Fade Animation on Navigation Bar
+
+const navLinksHoverAnimation = function (e) {
+  console.log(this, e.currentTarget);
+  if (e.target.classList.contains('nav__link')) {
+    const linkOver = e.target;
+    const siblingLinks = linkOver
+      .closest('.nav__links')
+      .querySelectorAll('.nav__link');
+    const logo = linkOver.closest('.nav').querySelector('img');
+    const logoText = linkOver.closest('.nav').querySelector('.nav__text');
+
+    siblingLinks.forEach(el => {
+      if (el !== linkOver) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+    logoText.style.opacity = this;
+  }
+};
